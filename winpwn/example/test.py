3862f0d12bed40c9c119ded4a022fcbc1c8e830e
Byzero512 <36794026+Byzero512@users.noreply.github.com>
2019-09-04 22:35:56 +0800

    Update test.py

--- winpwn/example/test.py
+++ winpwn/example/test.py
@@ -1,16 +1,19 @@
 from winpwn import *
 context.log_level='debug'
 context.arch='i386'
-# context.terminal=['ConEmu.exe']
-context.timeout=10000
-is_debug=1
+# context.timeout=10000
+is_debug=0
 if is_debug:
     if context.arch=='amd64':
         p=process('./dotest64.exe')
         gdb.attach(p) 
+        # windbg.attach(p)
+        # x64dbg.attach(p)
     else:
         p=process('./dotest.exe')
         gdb.attach(p)
+        # windbg.attach(p)
+        # x64dbg.attach(p)
 else:
     if context.arch=='amd64':
         p=process('./dotest64.exe')
