8ad761e3612f35333b5188c4f37535c1fa38cb81
Byzero512 <36794026+Byzero512@users.noreply.github.com>
2019-09-30 22:02:23 +0800

    Update setup.py

--- winpwn_nodeps/setup.py
+++ winpwn_nodeps/setup.py
@@ -2,7 +2,7 @@ from setuptools import *
 
 setup(
     name='winpwn',
-    version='0.0.6',
+    version='0.0.7',
     description='mini pwntools for windows',
     author='byzero512',
     author_email='byzero512@gamil.com',
